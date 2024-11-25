ce7fa4417cab3d1dff7cccf7e8024564f89c3d4e
Tom Krenzke <krenzke@gmail.com>
2024-11-25 09:44:25 -0800

    get images working in side project items

--- config.rb
+++ config.rb
@@ -43,5 +43,5 @@ page '/*.txt', layout: false
 configure :build do
   # activate :minify_css
   # activate :minify_javascript, compressor: Terser.new
-  activate :asset_hash
+  # activate :asset_hash
 end
